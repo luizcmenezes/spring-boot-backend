d72b47bc7494524e50f2bafeef2cd2cabcd68be5
Luiz Carlos Menezes Santos <luiz.santos@paygo.com.br>
2021-12-03 08:27:25 -0300

    Criando operação de instaciação;

---
+++ src/main/java/com/br/luiz/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,15 @@
+package com.br.luiz.cursomc.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.br.luiz.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+	Optional<Categoria> findById(Integer id);
+
+}
---
+++ src/main/java/com/br/luiz/cursomc/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.br.luiz.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.br.luiz.cursomc.domain.Categoria;
+import com.br.luiz.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+
+	public Categoria findById(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);		
+		return obj.orElse(null);
+		
+	}
+}
