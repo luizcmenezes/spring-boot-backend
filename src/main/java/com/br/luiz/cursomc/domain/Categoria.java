8aa6718f3333ca81c6c6d87d40b6c0cc61ebc942
Luiz Carlos Menezes Santos <lcmscontas@gmail.com>
2021-12-07 18:26:46 -0300

    Ajuste no end-point /categorias/{id};

--- src/main/java/com/br/luiz/cursomc/domain/Categoria.java
+++ src/main/java/com/br/luiz/cursomc/domain/Categoria.java
@@ -1,12 +1,17 @@
 package com.br.luiz.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Categoria implements Serializable {
@@ -18,6 +23,10 @@ public class Categoria implements Serializable {
 	private Integer id;
 	private String nome;
 
+	@JsonManagedReference
+	@ManyToMany(mappedBy = "categorias")
+	private List<Produto> produtos = new ArrayList<>();
+	
 	public Categoria() {
 	}
 
@@ -42,6 +51,14 @@ public class Categoria implements Serializable {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
 
 	@Override
 	public int hashCode() {
@@ -59,5 +76,6 @@ public class Categoria implements Serializable {
 		Categoria other = (Categoria) obj;
 		return Objects.equals(id, other.id);
 	}
+
 
 }
--- src/main/java/com/br/luiz/cursomc/services/CategoriaService.java
+++ src/main/java/com/br/luiz/cursomc/services/CategoriaService.java
@@ -7,16 +7,18 @@ import org.springframework.stereotype.Service;
 
 import com.br.luiz.cursomc.domain.Categoria;
 import com.br.luiz.cursomc.repositories.CategoriaRepository;
+import com.br.luiz.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
 
 	public Categoria findById(Integer id) {
-		Optional<Categoria> obj = repo.findById(id);		
-		return obj.orElse(null);
-		
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName(), null));
+
 	}
 }
